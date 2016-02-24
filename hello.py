194eeb1d18cb97f3ba04242eb37ce71d6f676eee
Progman-DT <progman.dt@gmail.com>
2016-02-25 00:48:12 +0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,5 +2,5 @@
 def application(environ, start_response):
   start_response('200 OK', [('Content-Type', 'text/plain')])
   qs = environ['QUERY_STRING']
-  ls = qs.split('&')
+  ls = qs.split("&")
   return ls
