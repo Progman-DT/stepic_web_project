1988b17ee5c4447a399d6aadc7beee29f8fbf25d
Progman-DT <progman.dt@gmail.com>
2016-03-03 20:24:19 +0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,5 +2,3 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 #sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
-sudo gunicorn -b 0.0.0.0:8080 hello:application
-sudo gunicorn -b 0.0.0.0:8000 ask/ask/wsgi:application 
