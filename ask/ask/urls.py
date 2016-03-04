20e0554e867803db9f6c8d001180a6151c75d912
Progman-DT <progman.dt@gmail.com>
2016-03-04 09:04:01 +0000

    From stepic

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,16 +1,20 @@
 from django.conf.urls import patterns, include, url
+
 from django.contrib import admin
+admin.autodiscover()
+from qa.views import test
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ask.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-# url(r'^$', include(admin.site.urls)),
-#    url(r'^login/', include(admin.site.urls)),
-#    url(r'^signup/', include(admin.site.urls)),
-#    url(r'^question/\d+/', test, name='test'),
-#    url(r'^ask/', include(admin.site.urls)),
-#    url(r'^popular/', include(admin.site.urls)),
-#    url(r'^new/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', test),
+    url(r'^login/', test),
+    url(r'^signup/', test),
+    url(r'^ask/', test),
+    url(r'^popular/', test),
+    url(r'^new/', test),
+    url(r'^question/\d+/', test),
 )
