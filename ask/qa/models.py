eacfdf5346ad7d1a6e337dc4a15ff820c82031f0
Progman-DT <progman.dt@gmail.com>
2016-03-07 13:47:11 +0500

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -7,7 +7,7 @@ class Question(models.Model):
 	title = models.CharField(max_length=255)
 	text = models.TextField()
 	added_at = models.DateTimeField(blank=True)
-	rating = models.BigIntegerField()
+	rating = models.IntegerField()
 	author = models.CharField(max_length=255)
 	likes = models.ManyToManyField(User)
 
