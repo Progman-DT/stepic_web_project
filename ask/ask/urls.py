265ed866dc502aeb35f55c2a5d6055a01230f63c
Progman-DT <progman.dt@gmail.com>
2016-03-08 23:11:29 +0500

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^/', include('qa.urls')),
+    url(r'^', include('qa.urls')),
     url(r'^admin/', include('admin.site.urls')),
 ]
